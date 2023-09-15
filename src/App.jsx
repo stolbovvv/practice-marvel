7ab67df12bb9797fb151138b34ed55ca9135e078
Valerii Stolbov <stolbov.v@outlook.com>
2023-09-15 16:01:22 +0500

    create slider & add outline-offset

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { RandomCharacter } from './components/random-character/random-character'
 import { SiteFooter } from './components/site-footer/site-footer';
 import { SiteHeader } from './components/site-header/site-header';
 import { SiteHero } from './components/site-hero/site-hero';
+import { Slider } from './components/slider/slider';
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
       <main className="main">
         <SiteHero title={'Marvel Data Base'} />
         <RandomCharacter />
+        <Slider title={'Latest Charcaters'} />
+        <Slider title={'Latest Comics'} />
       </main>
       <SiteFooter />
     </>
