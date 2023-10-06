117224fb8dc8dd3f92a4ef6a1f3506f3565819c7
Valerii Stolbov <stolbov.v@outlook.com>
2023-10-06 16:15:40 +0500

    Updates

---
+++ assets/site-hero-4def3660.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-ea8dcb4c.js";function i({title:s}){return e.jsx("section",{className:"site-hero",children:e.jsx("div",{className:"container site-hero__container",children:e.jsx("h1",{className:"site-hero__title",children:s})})})}export{i as S};
